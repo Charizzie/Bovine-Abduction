92d1396e2a4e8e1d99b90fe6121ba5eeb852d311
Liz <stokesec@hotmail.com>
2021-10-28 14:43:55 +1300

    Core Scripts Fin

---
+++ Assets/Liz/Scripts/Score.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text scoreText = null;
+
+    int score = 0;
+
+    public GameObject player;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Cow")
+        {
+            score++;
+
+        }
+    }
+}
